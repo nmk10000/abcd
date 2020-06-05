fdef9f9b587833d55d32b23720d5624e09306c96
nmk10000 <58908219+nmk10000@users.noreply.github.com>
2020-06-05 16:05:07 +0530

    Update echo.py

--- echo.py
+++ echo.py
@@ -2,6 +2,12 @@ import telebot
 
 bot = telebot.TeleBot("1296488130:AAH_Qi3WpsvlHy8Q9A-CEqib1TLjSxIw0kI")
 
-@bot.message_handler(func=lambda m: True)
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
+
+@bot.message_handler(func=lambda message: True)
 def echo_all(message):
-  bot.reply_to(message, message.text)
+	bot.reply_to(message, message.text)
+
+bot.polling()
