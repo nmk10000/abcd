43bac30cea512dcf9f1098f9679ba917e862541c
nmk10000 <58908219+nmk10000@users.noreply.github.com>
2020-06-05 16:14:31 +0530

    Update echo.py

--- echo.py
+++ echo.py
@@ -1,13 +1,29 @@
-import telebot
+from telebot import types
 
 bot = telebot.TeleBot("1296488130:AAH_Qi3WpsvlHy8Q9A-CEqib1TLjSxIw0kI")
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+# Using the ReplyKeyboardMarkup class
+# It's constructor can take the following optional arguments:
+# - resize_keyboard: True/False (default False)
+# - one_time_keyboard: True/False (default False)
+# - selective: True/False (default False)
+# - row_width: integer (default 3)
+# row_width is used in combination with the add() function.
+# It defines how many buttons are fit on each row before continuing on the next row.
+markup = types.ReplyKeyboardMarkup(row_width=2)
+itembtn1 = types.KeyboardButton('a')
+itembtn2 = types.KeyboardButton('v')
+itembtn3 = types.KeyboardButton('d')
+markup.add(itembtn1, itembtn2, itembtn3)
+tb.send_message(chat_id, "Choose one letter:", reply_markup=markup)
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-	bot.reply_to(message, message.text)
-
-bot.polling()
+# or add KeyboardButton one row at a time:
+markup = types.ReplyKeyboardMarkup()
+itembtna = types.KeyboardButton('a')
+itembtnv = types.KeyboardButton('v')
+itembtnc = types.KeyboardButton('c')
+itembtnd = types.KeyboardButton('d')
+itembtne = types.KeyboardButton('e')
+markup.row(itembtna, itembtnv)
+markup.row(itembtnc, itembtnd, itembtne)
+tb.send_message(chat_id, "Choose one letter:", reply_markup=markup)
